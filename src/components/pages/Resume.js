5ff70a5d1f5818bd3f9a66129a37332e07fd25a9
Bill Pate <wtpate69@hotmail.com>
2021-07-28 14:07:04 -0400

    Add proficiencies to Resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,8 +8,32 @@ export default function Resume() {
   return (
     <div className="container-lg">
       <br></br>
+      <h2>Resume</h2>
       <br></br>
       <a href={'https://drive.google.com/file/d/1xWtYvc8htPDIWsXh4eu4aJK50WeHWnA1/view?usp=sharing'}><h2>Download my resume</h2></a>
+      <br></br>
+      <h2>Front-end Proficiencies</h2>
+      <ol>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>Responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ol>
+      <h2>Back-end Proficiencies</h2>
+      <ol>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoos</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ol>
+      <br></br>
+
       <div className="text-center"  style={footStyle}>
         <span><a href="https://github.com/minprocess"><FaGithub size='40px'/></a></span>
         <span style={spanStyle}><a href="https://linkedin.com/in/bill-pate"><FaLinkedin size='40px'/></a></span>
